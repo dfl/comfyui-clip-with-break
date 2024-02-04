4b2b20e947ab4d5402547c7403370aa04693cbc9
David Löwenfels <282+dfl@users.noreply.github.com>
2024-02-04 09:56:12 +0000

    simplify by reusing native nodes

--- nodes.py
+++ nodes.py
@@ -1,5 +1,5 @@
 import re
-
+import nodes as native
 class CLIPTextEncodeWithBreak:
     @classmethod
     def INPUT_TYPES(s):
@@ -11,31 +11,15 @@ class CLIPTextEncodeWithBreak:
 
     def encode(self, clip, text):
         prompts = re.split(r"\s*\bBREAK\b\s*", text) 
+        # encode first prompt fragment
         prompt = prompts.pop(0)
-        tokens = clip.tokenize(prompt)
-        cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
-        out = [[cond, {"pooled_output": pooled}]]
-
+        print(f"prompt: {prompt}")
+        out = native.CLIPTextEncode.encode(self, clip, prompt)
+        # encode and concatenate the rest of the prompt
         for prompt in prompts:
-            conditioning_from = out
-            tokens = clip.tokenize(prompt)
-            cond, pooled = clip.encode_from_tokens(tokens, return_pooled=True)
-            conditioning_to = [[cond, {"pooled_output": pooled}]]
-
-            # def concat(self, conditioning_to, conditioning_from):
-            out = []
-
-            if len(conditioning_from) > 1:
-                print("Warning: ConditioningConcat conditioning_from contains more than 1 cond, only the first one will actually be applied to conditioning_to.")
-
-            cond_from = conditioning_from[0][0]
-
-            for i in range(len(conditioning_to)):
-                t1 = conditioning_to[i][0]
-                tw = torch.cat((t1, cond_from),1)
-                n = [tw, conditioning_to[i][1].copy()]
-                out.append(n)
-        return (out, )
+            cond_to = native.CLIPTextEncode.encode(self, clip, prompt)
+            out = native.ConditioningConcat.concat(self, cond_to[0], out[0])
+        return out
 
 
 # A dictionary that contains all nodes you want to export with their names
